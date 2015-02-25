d8f829fa18cf873ecea513edca4501c474bf78f4
Victor Holban <victor.holban@gmail.com>
2015-02-25 11:42:42 +0200

    Add seeInLastMail and dontSeeInLastEmail

--- src/GMailAPI.php
+++ src/GMailAPI.php
@@ -25,6 +25,36 @@ class GMailAPI extends Module
     }
 
     /**
+     * See In Last Email
+     *
+     * Look for a string in the most recent email
+     *
+     * @param $expected string
+     * @return void
+     **/
+    public function seeInLastEmail($expected)
+    {
+        $email = $this->lastMessage();
+        $this->seeInEmail($email, $expected);
+    }
+
+
+    /**
+     * Don't See In Last Email
+     *
+     * Look for the absence of a string in the most recent email
+     *
+     * @param $unexpected string
+     * @return void
+     **/
+    public function dontSeeInLastEmail($unexpected)
+    {
+        $email = $this->lastMessage();
+        $this->dontSeeInEmail($email, $unexpected);
+    }
+
+
+    /**
      * See In Last Email subject
      *
      * Look for a string in the most recent email subject
@@ -37,6 +67,7 @@ class GMailAPI extends Module
         $email = $this->lastMessage();
         $this->seeInEmailSubject($email, $expected);
     }
+
 
     /**
      * Don't See In Last Email subject
@@ -58,10 +89,10 @@ class GMailAPI extends Module
      *
      * Get the most recent email
      *
-     * @return /Google_Service_Gmail_Message
+     * @return \Google_Service_Gmail_Message
      **/
     protected function lastMessage() {
-        $messages = $this->messages(1);
+        $messages = $this->messages(array('maxResults' => 1));
         if (empty($messages)) {
             $this->fail("No messages received");
         }
@@ -77,12 +108,14 @@ class GMailAPI extends Module
      *
      * Get an array of all the message objects
      *
-     * @param $maxResults int
+     * @param $params array
      * @return array
      **/
-    protected function messages($maxResults = 100) {
+    protected function messages($params = array()) {
+        $defaultParam = array('maxResults' => 100);
+        $params = array_merge($defaultParam, $params);
         /** @var /Google_Service_Gmail_ListMessagesResponse $list */
-        $list = $this->service->users_messages->listUsersMessages('me',['maxResults' => $maxResults]);
+        $list = $this->service->users_messages->listUsersMessages('me', $params);
         return $list->getMessages();
     }
 
@@ -91,10 +124,39 @@ class GMailAPI extends Module
      *
      * Given a GMail id, returns the email's object
      *
-     * @return /Google_Service_Gmail_Message
+     * @param $id string
+     * @return \Google_Service_Gmail_Message
      **/
     protected function emailFromId($id) {
-        return $this->service->users_messages->get('me',$id,['format' => 'full']);
+        return $this->service->users_messages->get('me', $id, array('format' => 'full'));
+    }
+
+    /**
+     * See In Email
+     *
+     * Look for a string in an email
+     *
+     * @param $email /Google_Service_Gmail_Message
+     * @param $expected string
+     * @return void
+     **/
+    protected function seeInEmail($email, $expected)
+    {
+        $this->assertContains($expected, $this->getEmailContent($email), "Email Contains");
+    }
+
+    /**
+     * Don't See In Email
+     *
+     * Look for the absence of a string in an email
+     *
+     * @param $email /Google_Service_Gmail_Message
+     * @param $unexpected string
+     * @return void
+     **/
+    protected function dontSeeInEmail($email, $unexpected)
+    {
+        $this->assertNotContains($unexpected, $this->getEmailContent($email), "Email Does Not Contain");
     }
 
     /**
@@ -124,16 +186,67 @@ class GMailAPI extends Module
         $this->assertNotContains($unexpected, $this->getEmailHeader($email, 'Subject'), "Email Subject Does Not Contain");
     }
 
+    /********************
+     *  Helper function
+     ********************/
+
     /**
      * @param $email /Google_Service_Gmail_Message
      * @param $headerName string
      * @return string
      */
     protected function getEmailHeader($email, $headerName) {
-        foreach ($email->getPayload()->getHeaders() as $header) {
+        return $this->getHeaderValue($email->getPayload()->getHeaders(), $headerName);
+    }
+
+    /**
+     * @param $headers array
+     * @param $headerName string
+     * @return string
+     */
+    protected function getHeaderValue($headers, $headerName) {
+        foreach ($headers as $header) {
+            if (!isset($header['name']) || !isset($header['value'])) continue;
             if ($header['name'] == $headerName) return $header['value'];
         }
         return '';
+    }
+
+    /**
+     * @param $email /Google_Service_Gmail_Message
+     * @param $type string 'html' | 'plain'
+     * @return string
+     */
+    protected function getEmailContent($email, $type = 'plain') {
+        if(!in_array($type, array('html', 'plain'))) {
+            $type = 'plain';
+        }
+
+        foreach($email->getPayload()->getParts() as $emailPart) {
+            if ($emailPart->mimeType != 'text/'.$type) continue;
+            return $this->base64url_decode($emailPart['body']['data']);
+        }
+        return '';
+    }
+
+    /**
+     * Custom base64 encode function required by GMail API
+     *
+     * @param $data
+     * @return string
+     */
+    protected function base64url_encode($data) {
+        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+    }
+
+    /**
+     * Custom base64 decode function required by GMail API
+     *
+     * @param $data
+     * @return string
+     */
+    protected function base64url_decode($data) {
+        return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
     }
 
     public function _refreshToken() {
